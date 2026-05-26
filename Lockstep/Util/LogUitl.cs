b94c5b25370c0833bee1e2f89860525baab14294
djaiz <djaizmassi@gmail.com>
2026-05-26 13:18:14 -0400

    Configure basic logging

---
+++ Lockstep/Util/LogUitl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Lockstep.Util;
+
+internal static class LogUitl
+{
+    public static ILogger CreateLogger(string name)
+    {
+        using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddSimpleConsole(options =>
+            {
+                options.SingleLine = true;
+                options.TimestampFormat = "HH:mm:ss ";
+            });
+
+            builder.SetMinimumLevel(LogLevel.Debug);
+        });
+
+        return loggerFactory.CreateLogger(name);
+    }
+}
