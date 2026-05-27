8e6e1042e308b4e505003ca1e74ab4a974f7c4b5
djaiz <djaizmassi@gmail.com>
2026-05-27 00:32:40 -0400

    Update Packet type to more genreic payload type

---
+++ Lockstep/Protocol/Payload.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace Lockstep.Protocol;
+
+internal readonly ref struct Payload
+{
+    public readonly ReadOnlySpan<byte> Buffer;
+    public readonly EndPoint Sender;
+
+    public Payload(ReadOnlySpan<byte> payload, EndPoint sender)
+    {
+        Buffer = payload;
+        Sender = sender;
+    }
+
+    public Payload(Payload other, int startOffset)
+    {
+        Buffer = other.Buffer[startOffset..];
+        Sender = other.Sender;
+    }
+}
