a8097802a0e6c5f49d59d91dcd09f0c5e68eb8ad
djaiz <djaizmassi@gmail.com>
2026-05-26 22:07:52 -0400

    Use singleton logger

---
+++ Lockstep/Util/Logger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Lockstep.Util;
+
+internal static class Logger
+{
+    private static ILogger _logger = null!;
+    private static readonly Lock _lock = new Lock();
+
+    public static ILogger Get()
+    {
+        if (_logger == null)
+        {
+            lock (_lock)
+            {
+                if (_logger == null)
+                {
+                    using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+                    {
+                        builder.AddSimpleConsole(options =>
+                        {
+                            options.SingleLine = true;
+                            options.TimestampFormat = "HH:mm:ss ";
+                        });
+
+                        builder.SetMinimumLevel(LogLevel.Debug);
+                    });
+
+                    _logger = loggerFactory.CreateLogger("Server");
+                }
+            }
+        }
+
+        return _logger;
+    }
+}
