87ddda0b625dd7139f3160ef4d1ab3df992378db
djaiz <djaizmassi@gmail.com>
2026-05-30 16:05:10 -0400

    Add pending packet type

---
+++ Lockstep/Protocol/PendingPacket.cs
@@ -0,0 +1,23 @@
+﻿namespace Lockstep.Protocol;
+
+internal class PendingPacket
+{
+    private int _triesLeft;
+
+    public DateTime LastSent { get; private set; } = DateTime.UtcNow; 
+    public int SequenceNumber { get; init; }
+    public int TriesLeft => _triesLeft;
+
+    public void UpdateTime()
+    {
+        LastSent = DateTime.UtcNow;
+    }
+
+    public void DecrementTries()
+    {
+        if (_triesLeft > 0)
+        {
+            _triesLeft--;
+        }
+    }
+}
