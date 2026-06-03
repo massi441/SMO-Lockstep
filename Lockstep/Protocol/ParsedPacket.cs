385e841d72c21692325d9f60db5217762e8a0029
djaiz <djaizmassi@gmail.com>
2026-06-02 22:49:45 -0400

    Update ParsedPacket to return a reference view of the header into the packet

--- Lockstep/Protocol/ParsedPacket.cs
+++ Lockstep/Protocol/ParsedPacket.cs
@@ -9,9 +9,23 @@ namespace Lockstep.Protocol;
 /// </summary>
 internal readonly struct ParsedPacket
 {
+    /// <summary>
+    /// The sender of the packet
+    /// </summary>
     public required IPEndPoint Sender { get; init; }
+
+    /// <summary>
+    /// The rented buffer of the packet
+    /// </summary>
     public required RentedBuffer<byte> RentedBuffer { get; init; }
 
-    public PacketHeader Header => MemoryMarshal.Read<PacketHeader>(RentedBuffer.Ref.AsSpan());
+    /// <summary>
+    /// Returns a view of the header inside the packet's payload
+    /// </summary>
+    public ref PacketHeader Header => ref MemoryMarshal.AsRef<PacketHeader>(RentedBuffer.Ref.AsSpan());
+
+    /// <summary>
+    /// Returns a span of the payload of the packet
+    /// </summary>
     public ReadOnlySpan<byte> Payload => RentedBuffer.Ref.AsSpan(PacketHeader.SizeOf(), Header.PayloadSize);
 }
