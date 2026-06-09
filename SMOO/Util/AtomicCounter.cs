ee085cc6ccf9d1c7c6e9cc803536f2bcda15f127
djaiz <djaizmassi@gmail.com>
2026-06-09 18:03:21 -0400

    Add atomic counter for storing references

---
+++ SMOO/Util/AtomicCounter.cs
@@ -0,0 +1,23 @@
+﻿namespace SMOO.Util;
+
+internal class AtomicCounter
+{
+    private int _count = 0;
+
+    public int Count => _count;
+
+    public int Increment()
+    {
+        return Interlocked.Increment(ref _count);
+    }
+
+    public int Decrement()
+    {
+        if (_count == 0)
+        {
+            return _count;
+        }
+
+        return Interlocked.Decrement(ref _count);
+    }
+}
